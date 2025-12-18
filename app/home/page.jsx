d58b31c3614309eca74bf61517d592d3f601a56d
UnstableBlob <atharva20453@gmail.com>
2025-12-19 00:55:10 +0530

    HomePage

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -2,14 +2,12 @@
 
 import Navbar from "@/components/Home/Navbar";
 import Hero from "@/components/HomePage/Hero";
-
 export default function Home() {
   // Lenis is initialized in the client-only component below
 
   return (
     <>
       <div>
-        <Navbar />
         <Hero />
       </div>
     </>
